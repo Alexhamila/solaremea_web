e81bfd2889a1ea5cc00cb640357187407817d531
Alexhamila <alexhamiladeveloper@gmail.com>
2020-10-02 11:36:36 +0200

    upd

--- pages/index.js
+++ pages/index.js
@@ -36,10 +36,10 @@ export default function Home() {
           <div className="bg">
             <img src="/head.jpg" alt="background image solaremea"></img>
           </div>
-          <div className="contain headContent">
+          <div className="headContent contain">
             <h1 className="huge">Join the solar energy revolution</h1>
-            <p>We're transforming access to renewable energy with simple, powerful solar solutions.</p>
-            <a>Learn More</a>
+            <p className="underP">We're transforming access to renewable energy with simple, powerful solar solutions.</p>
+            <a className="btnRound">Learn More</a>
           </div>
         </section>
 
@@ -54,6 +54,44 @@ export default function Home() {
             </ul>
           </nav>
           <div className="contain">
+            <div className="solutions">
+              <div className="solutionsPresentation">
+                <h2>Solutions</h2>
+                <p className="underP">Our innovative community solar & project development solutions make solar possible for households, businesses, property owners, and more.</p>
+              </div>
+              
+              <div class="row">
+                <div class="col-lg">
+                  <div className="content">
+                    <div className="heading">
+                      <img style={{marginRight: '1.5em'}} src="https://bluewavesolar.com/resources/img/icons/community_solar.min.svg" alt="Community Solar"/>
+                      <h3 style={{margin: '0'}}className="mainTitle">Community Solar</h3>
+                    </div>
+                    <p className="underP">Breaking down barriers to solar benefits. Homeowners, businesses, and towns can enjoy savings by signing up for a community solar share. No rooftop panels required!</p>
+                    <a className="btnRound">Learn More</a>
+                    <ul className="solutionLinks">
+                      <li>
+                        <img src="https://bluewavesolar.com/resources/img/icons/solutions/person.min.svg" alt="Community Solar Project Services"/>
+                        <div>
+                          <a>Community Solar Project Services</a>
+                          <p>You develop the project, let our team acquire and manage the customers.</p>
+                        </div>  
+                      </li>
+                      <li>
+                        <img src="https://bluewavesolar.com/resources/img/icons/solutions/person.min.svg" alt="Community Solar Sales Partners"/>
+                        <div>
+                          <a>Community Solar Sales Partners</a>
+                          <p>Broaden your customer base with access to the solar solution for more people in more places.</p>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-lg" style={{textAlign: "center"}}>
+                  <img class="mainImg" src="/community_solar.jpg" alt="Community_solar"/>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
 
