e2703f299b7570b627c31bd0926bd9c47ecd65ea
Alexhamila <alexhamiladeveloper@gmail.com>
2020-10-02 10:49:25 +0200

    up

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,63 @@
 import Head from 'next/head'
+import { Button, Container} from 'react-bootstrap'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Home - Solaremea</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="pageContain">
+        <header className="header">
+          <div className="contain">
+            <a className="logo" href="https://solaremea.net/">
+                SOLAREMEA
+            </a>
+            <nav className="mainNav">
+              <ul className="nav">
+                <li>
+                  <span className="navItem">Solutions</span>
+                </li>
+                <li>
+                  <span className="navItem">Company</span>
+                </li>
+                <li>
+                  <span className="navItem">Resources</span>
+                </li>
+              </ul>
+            </nav>
+            <a href="/login" className="loginBtn">Log In</a>
+          </div>
+        </header>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <section className="head">
+          <div className="bg">
+            <img src="/head.jpg" alt="background image solaremea"></img>
+          </div>
+          <div className="contain headContent">
+            <h1 className="huge">Join the solar energy revolution</h1>
+            <p>We're transforming access to renewable energy with simple, powerful solar solutions.</p>
+            <a>Learn More</a>
+          </div>
+        </section>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <section className="bodyContent">
+          <nav className="pageNav blockContain">
+            <ul>
+              <li><a href="#solutions" className="subNav">Solutions</a></li>
+              <li><a href="#getstarted" className="subNav">Get Started</a></li>
+              <li><a href="#about" className="subNav">About</a></li>
+              <li><a href="#featured" className="subNav">Featured</a></li>
+              <li><a href="#contact" className="subNav">Contact</a></li>
+            </ul>
+          </nav>
+          <div className="contain">
+          </div>
+        </section>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   )
 }
